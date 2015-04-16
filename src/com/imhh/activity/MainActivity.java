98efabaa2d6b320b1ee827eca5ef652a54cad282
lhhh <597776449@qq.com>
2015-04-16 19:19:28 +0800

    dd
    1.1

--- src/com/imhh/activity/MainActivity.java
+++ src/com/imhh/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Toast.makeText(getApplicationContext(), "46456", 1).show();
 	}
 
 	@Override
